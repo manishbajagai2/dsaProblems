ed6b91bd14cee20a0ed07df23def655ba30780c2
manishbajagai2 <manishbajagai2@gmail.com>
2022-02-12 23:41:27 +0530

    Array

---
+++ rearrangePosAndNeg.cpp
@@ -0,0 +1,64 @@
+/*
+Rearrange array in alternating positive & negative items with O(1) extra space
+
+Examples :
+
+Input:  arr[] = {1, 2, 3, -4, -1, 4}
+Output: arr[] = {-4, 1, -1, 2, 3, 4}
+
+Input:  arr[] = {-5, -2, 5, 2, 4, 7, 1, 8, 0, -8}
+output: arr[] = {-5, 5, -2, 2, -8, 4, 7, 1, 8, 0}
+
+Relative order is maintained
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void rearrange(int arr[], int n)
+{
+    int i = -1;
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[j] < 0)
+        {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    int pos = i + 1, neg = 0;
+    while (pos < n && neg < pos && arr[neg] < 0)
+    {
+        swap(&arr[neg], &arr[pos]);
+        pos++;
+        neg += 2;
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    int arr[] = {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout<<"\nOriginal array is : ";
+    printArray(arr, n);
+    rearrange(arr, n);
+    cout<<"\nRearranged array is : ";
+    printArray(arr, n);
+    cout<<endl;
+    return 0;
+}
