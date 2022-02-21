07428d0935b146f9f95330cafc4f366e7c899fa5
manishbajagai2 <manishbajagai2@gmail.com>
2022-02-21 23:09:54 +0530

    Array

---
+++ medianSameSize.cpp
@@ -0,0 +1,23 @@
+/*
+Median of two sorted arrays of same size
+
+Input: 
+ar1[] = {1, 12, 15, 26, 38}
+ar2[] = {2, 13, 17, 30, 45}
+
+Output: 
+16
+
+Explanation:
+Middle two elements are 15 and 17 Average of middle elements is (15 + 17) / 2 which is equal to 16
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int ar1[] = {1, 12, 15, 26, 38};
+    int ar2[] = {2, 13, 17, 30, 45};
+    int n = sizeof(ar1) / sizeof(ar1[0]);
+    return 0;
+}
